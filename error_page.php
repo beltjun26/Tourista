d0269a495e460930f1f79ab9f6754ff7cf05390e
XonGado <cjubs.delgado@gmail.com>
2016-11-23 04:22:44 +0800

    Made error page, fixed navbar bug. Linked every logout link to logut.php

--- error_page.php
+++ error_page.php
@@ -1,3 +1,51 @@
-Sorry that page can't be displayed.
+<!-- THIS HAS NO NAV BAR -->
+<?php
+	session_start();  
+	if(!isset($_SESSION["userName"])){ 
+		header('location:login.php');
+	}
+	$username = $_SESSION["userName"];
 
-<a href="home_page.php">Go back to homepage.</a>
+	/*$query = "SELECT * FROM account WHERE username='{$_SESSION['username']}';";
+	$result= mysqli_query($connect, $query);
+	$row = mysqli_fetch_assoc($result);
+	$_SESSION["userID"] = $row['acc_id'];*/
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>Toursita</title>
+		<link rel="shortcut icon" href="images/Tourista_Logo_Outline_blue.ico"/>
+		<meta name="James Anthony Yatar" content="Navigation Bar">
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+		<script src="bootstrap/jquery/1.12.4/jquery.min.js"></script>
+		<script src="bootstrap/js/bootstrap.min.js"></script>
+		<script type="text/javascript" src="js/jquery-3.1.1.min.js"></script>
+		<link rel="stylesheet" type="text/css" href="css/navigation_bar_and_body_style.css">
+		<link rel="stylesheet" type="text/css" href="css/error.css">
+	</head>
+	<body>
+		<div id = "navBar">
+			<form action="search_results_places.php" method="get">
+				<input type="text" placeholder="Search..." name = "search">
+				<!-- <input type="submit" value="SEARCH" > -->
+			</form>
+			<ul id = "navList">
+				<li><a href="#" class="active"><span class="glyphicon glyphicon-home"></span>HOME</a></li>
+				<li><a href="visit.php"><span class="glyphicon glyphicon-map-marker"></span>VISITS</a></li>
+				<li><a href="#"><span class="glyphicon glyphicon-globe"></span>EXPLORE</a></li>
+				<li><a href="Notifications.php"><span class="glyphicon glyphicon-bell"></span>NOTIFICATIONS</a></li>
+				<li><a href="logout.php"><span class="glyphicon glyphicon-log-out"></span>LOGOUT</a></li>
+				<li><a href="my_profile.php" class="image-list"><img src="images/profile_pic_img/acc_id_<?=$_SESSION['userID']?>.jpg"></a></li>
+			</ul>
+		</div>
+		<div class="container">
+			<span class="error">ERROR</span>
+			<p>Sorry that page can't be displayed.</p>
+			<a href="home_page.php">Go back to homepage.</a>
+		</div>
+	</body>
+</html>
